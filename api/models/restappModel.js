fd441ce5f2b02974d9b0a05d4295ec51a9dc8e39
Anubhav Guha <anubhav.guha@sap.com>
2021-07-07 21:07:41 +0000

    model changes

--- api/models/restappModel.js
+++ api/models/restappModel.js
@@ -9,27 +9,27 @@
     const SalesOrder = new Schema({
         serviceorder_id: {
             type:String,
-            required:true
+            
         },
         customer_id: {
             type:String,
-            required:true
+            
         },
         customer_type: {
             type:String,
-            required:true
+            
         },
         transaction_type: {
             type:String,
-            required:true
+            
         },
         status: {
             type:String,
-            required:true
+            
         },
         priority: {
             type:String,
-            required:true
+            
         },
         status:{
             type:Boolean,
@@ -41,39 +41,39 @@
         },
         serviceorders_total: {
             type:String,
-            required:true
+            
         },
         serviceorders_issue: {
             type:String,
-            required:true
+           
         },
         serviceorders_need_action: {
             type:String,
-            required:true
+            
         },
         street: {
             type:String,
-            required:true
+            
         },
         city: {
             type:String,
-            required:true
+            
         },
         state: {
             type:String,
-            required:true
+            
         },
         zip: {
             type:String,
-            required:true
+            
         },
         phone: {
             type:String,
-            required:true
+            
         },
         custname: {
             type:String,
-            required:true
+           
         }
     });
 
